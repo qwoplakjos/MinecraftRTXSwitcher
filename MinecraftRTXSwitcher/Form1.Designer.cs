8f0cb98a1ec5f1b156226525707f805754b78d27
qwoplakjos <qwoplakjos@gmail.com>
2025-04-03 01:30:23 +0300

    resized main form so its not annoying

--- MinecraftRTXSwitcher/Form1.Designer.cs
+++ MinecraftRTXSwitcher/Form1.Designer.cs
@@ -79,7 +79,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(236, 123);
+            this.ClientSize = new System.Drawing.Size(234, 123);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.enableButton);
             this.Controls.Add(this.disableButton);
